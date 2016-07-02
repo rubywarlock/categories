8a7e314165bbe0e2948ed209f143834bb743f082
Vitaliy <faetonvs@gmail.com>
2016-07-02 23:14:20 +0500

    add highlight to search

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -6,13 +6,13 @@ class SearchesController < ApplicationController
   end
 
   def find
-	  search = params[:p]
+	  @search = params[:p]
 	  #search.gsub!(/[\\]+|[\/]+|LIKE|\*|SELECT|WHERE|ALERT|DROP|TABLE/i,"")
 	  #@users = User.where(
 		#  "age LIKE ? OR pay LIKE ? OR stature LIKE ? OR weigh LIKE ? OR email LIKE ?",
 		#  "%#{search.to_i}%", "%#{search.to_i}%", "%#{search.to_i}%",
 		#  "%#{search.to_i}%", "%#{search}%").limit(10)
-	  @users = User.where("email LIKE ?", "%#{search}%").limit(10)
+	  @users = User.where("email LIKE ?", "%#{@search}%").limit(10)
 
     respond_to do |format|
       format.html
