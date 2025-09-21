2b302ba18889919eff452b0c307371c59b0debe1
Vitaly Simakov <vitaly.s.simakov@tele2.ru>
2025-09-21 19:14:31 +0500

    feature: check access to content

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,11 @@
 module ApplicationHelper
+  def current_user_have_access?(current_user, access_target)
+    return true if current_user&.admin?
 
+    if current_user
+      current_user&.id == access_target&.user_id
+    else
+      false
+    end
+  end
 end
