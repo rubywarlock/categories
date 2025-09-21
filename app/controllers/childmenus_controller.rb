55cbb4a0f570dfc141423af0bc6214046cc0ffb4
Vitaly Simakov <vitaly.s.simakov@tele2.ru>
2025-09-21 17:34:25 +0500

    feature: edit childmenu

--- app/controllers/childmenus_controller.rb
+++ app/controllers/childmenus_controller.rb
@@ -1,14 +1,14 @@
 class ChildmenusController < ApplicationController
+  before_action :set_childmenu, only: [:show, :edit, :update, :destroy]
+
   def show
-    @childmenu = Childmenu.find(params[:id])
   end
 
   def new
-    @childmenu = Childmenu.new(:mainmenu_id => params[:id])
+    @childmenu = Childmenu.new(mainmenu_id: params[:id])
   end
 
   def create
-    @childmenu = Childmenu.new(params.require(:childmenu).permit(:mainmenu_id, :title, :text))
     if @childmenu.save
       redirect_to @childmenu, notice: 'Childmenu was successfully updated.'
     else
@@ -21,15 +21,30 @@ class ChildmenusController < ApplicationController
   end
 
   def update
+
+    if @childmenu.update_attributes(childmenu_params)
+      redirect_to @childmenu, notice: 'Childmenu was successfully updated.'
+    else
+      render action: "edit"
+    end
   end
 
   def destroy
-    @delete = Childmenu.find(params[:id])
     if @delete.destroy
       redirect_to root_path#new_childmenus_path, notice: 'Childmenu was successfully deleted.'
     else
       flash[:success] = 'Delete Childmenu error'
       render 'new'
     end
+  end
+
+  private
+
+  def childmenu_params
+    params.require(:childmenu).permit(:mainmenu_id, :title, :text)
+  end
+
+  def set_childmenu
+    @childmenu = Childmenu.find(params[:id])
   end
 end
